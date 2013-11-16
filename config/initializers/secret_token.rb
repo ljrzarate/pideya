a35cc7d2ee697008beffddd9f0192ed6f8875967
Luis J Rodriguez <ljrzarate@gmail.com>
2013-11-16 09:00:06 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pideya::Application.config.secret_token = 'a0e7dfcee3f2ef67deed8f8959236156aef40edabd4008f97dd233cc8cec411158647de0c3d333246d1171611188e227a178651f02052acac79bb831d4d09403'
