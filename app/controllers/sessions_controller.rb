e6a20668d5e74db4501ad9064acc284ea37d9bd8
ivangranados <ivan.granados@koombea.com>
2013-11-16 21:36:16 -0500

    couples

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,6 @@
 class SessionsController < ApplicationController
   def create
       user = User.from_omniauth(params[:provider], env["omniauth.auth"])
-      user.couples.create()
       session[:user_id] = user.id
       redirect_to dashboard_path
   end
