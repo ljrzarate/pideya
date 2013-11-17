9c603b55d4084f5c2f624621ad9352fc75ca6987
ivangranados <ivan.granados@koombea.com>
2013-11-17 00:18:16 -0500

    images user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  attr_accessible :name, :oauth_expires_at, :oauth_token, :provider, :uid
+  attr_accessible :name, :oauth_expires_at, :oauth_token, :provider, :uid, :image
 
   def self.from_omniauth(provider, auth)
     user_session = SessionStrategy::UserSession.new provider
